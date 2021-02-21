d03a92ca54803a1fe652bcd6b801012ac0a0d406
liiniix <thaki240198cgc@gmail.com>
2021-02-21 21:40:17 +0600

    f1

--- dt.py
+++ dt.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 import numpy as np
+from pprint import pprint
 
 class Node:
 	def __init__(self):
@@ -41,10 +42,10 @@ class DT_Classifier:
 	def __init__(self):
 		self.root = Node()
 	
-	def read_dataset(self, data_path='heart.dat'):
-		self.dat = pd.read_csv(data_path, header=None, sep=' ').astype(str).sample(frac=1).reset_index(drop=True)
-		self.train = self.dat.head(int(self.dat.shape[0]*.8))
-		self.test = self.dat.tail(self.dat.shape[0]-int(self.dat.shape[0]*.2))
+	def read_dataset(self, data_path='nursery.data'):
+		self.dat = pd.read_csv(data_path, header=None).astype(str).sample(frac=1).reset_index(drop=True)
+		self.train = self.dat.head(int(self.dat.shape[0]*.7))
+		self.test = self.dat.tail(self.dat.shape[0]-int(self.dat.shape[0]*.7))
 	
 	def gini_calc(self, D, attr):
 		#print(D)
@@ -62,31 +63,51 @@ class DT_Classifier:
 		
 		gini = 1. - prob_1**2 - prob_2**2
 		return gini
-	
+
 	def attribute_selection(self, D):
 		smallest_gini = 10000.
 		smallest_gini_index = -1.
+		final_first_entry = ''
 		for i in D.columns[:-1]:
 			D.sort_values(i)
 			if len(D[i].unique())<20:
-				first_entry = D[i].unique()[0]
+				first_entry = np.sort(D[i].unique())[0]
+				
+				
+				
+				partition_1 = D[D[i]<=first_entry]
+				partition_2 = D[D[i]>first_entry]
+				
+				coeff = partition_1.shape[0]/D.shape[0]
+				gini = coeff*self.gini_calc(partition_1, i) + (1-coeff)*self.gini_calc(partition_2, i)
+				if gini<smallest_gini:
+					smallest_gini = gini
+					smallest_gini_index = i
+			
+				
+				
+				
+				
 				
 			else:
-				first_entry = D[i].iloc[int(D.shape[0]/2)]
+
+				for point in range(D[i].shape[0]):
+					first_entry = D[i].iloc[point]
 			
-			partition_1 = D[D[i]<=first_entry]
-			partition_2 = D[D[i]>first_entry]
+					partition_1 = D[D[i]<=first_entry]
+					partition_2 = D[D[i]>first_entry]
 				
-			coeff = partition_1.shape[0]/D.shape[0]
-			gini = coeff*self.gini_calc(partition_1, i) + (1-coeff)*self.gini_calc(partition_2, i)
-			if gini<smallest_gini:
-				smallest_gini = gini
-				smallest_gini_index = i
+					coeff = partition_1.shape[0]/D.shape[0]
+					gini = coeff*self.gini_calc(partition_1, i) + (1-coeff)*self.gini_calc(partition_2, i)
+					if gini<smallest_gini:
+						smallest_gini = gini
+						smallest_gini_index = i
+						final_first_entry = first_entry
 		D.sort_values(smallest_gini_index)
 		if len(D[smallest_gini_index].unique())>=20:
-			first_entry = D[smallest_gini_index].iloc[int(D.shape[1]/2)]
+			first_entry = final_first_entry
 		else:
-			first_entry = D[smallest_gini_index].unique()[0]
+			first_entry = np.sort(D[smallest_gini_index].unique())[0]
 			
 		return (smallest_gini_index, first_entry)
 	def Decision_Tree_Initiation(self):
@@ -98,9 +119,9 @@ class DT_Classifier:
 
 		node = Node()
 		# pruning wrt majority vote
-		if D.shape[1]<self.train.shape[1]*.1:
-			node.set_leaf(D.iloc[:,-1].value_counts().index[0])
-			return node
+		#if D.shape[1]<self.train.shape[1]*.0001:
+		#	node.set_leaf(D.iloc[:,-1].value_counts().index[0])
+		#	return node
 			
 		if len(D.iloc[:,-1].unique()) == 1:
 			node.set_leaf(D.iloc[:,-1].unique()[0])
@@ -154,9 +175,13 @@ class DT_Classifier:
 	def calc_precision_recall(self):
 		print(self.test.iloc[:,-1], self.predicted)
 		self.cm = confusion_matrix(self.test.iloc[:,-1], self.predicted, self.test.iloc[:,-1].unique())
-		self.recall = np.diag(self.cm) / np.sum(self.cm, axis = 1)
-		self.precision = np.diag(self.cm) / np.sum(self.cm, axis = 0)
-		print(self.cm, self.test.iloc[:,-1].unique())
+		self.recall_classwise = np.diag(self.cm) / (np.sum(self.cm, axis = 1) +1.e-8)
+		self.precision_classwise = np.diag(self.cm) / (np.sum(self.cm, axis = 0) +1.e-8)
+		self.recall = np.mean(self.recall_classwise)
+		self.precision = np.mean(self.precision_classwise)
+		self.f1 = 2.*self.precision*self.recall/(self.precision+self.recall)
+		pprint(self.cm)
+		print(self.precision, self.recall, self.f1)
 
 classifier = DT_Classifier()
 
@@ -171,6 +196,7 @@ def f(n):
 	if n is None:
 		return
 	if n.leaf == False:
+		print(n.index, n.left_class)
 		print('l')
 		f(n.left)
 		print('r')
--- nb2.py
+++ nb2.py
@@ -1,7 +1,8 @@
 import math
 data = 'kr-vs-kp.data'
 from pprint import pprint
-
+from sklearn.metrics import confusion_matrix
+import numpy as np
 import pandas as pd
 
 
@@ -9,20 +10,21 @@ class NB_Classifier:
 	def __init__(self):
 		pass
 	
-	def read_dataset(self, data_path='kr-vs-kp.data'):
-		self.dat = pd.read_csv(data_path, header=None).astype(str)
-	
+	def read_dataset(self, data_path='nursery.data'):
+		self.dat = pd.read_csv(data_path, header=None).astype(str).sample(frac=1).reset_index(drop=True)
+		self.train = self.dat.head(int(self.dat.shape[0]*.7))
+		self.test = self.dat.tail(self.dat.shape[0]-int(self.dat.shape[0]*.7))
 	
 	def summerize_by_class(self):
-		self.classes = self.dat.iloc[:,-1].unique()
+		self.classes = self.train.iloc[:,-1].unique()
 		print(self.classes)
 		
 		self.summary = dict()
 		
 		for a_class in self.classes:
-			separated_dataset = self.dat[self.dat.iloc[:,-1]==a_class]
+			separated_dataset = self.train[self.train.iloc[:,-1]==a_class]
 			if a_class not in self.summary:
-				self.summary[a_class] = dict()
+				self.summary[a_class] = []
 			
 			for column in range(separated_dataset.shape[1]-1):
 				self.summary[a_class].append(separated_dataset[column].value_counts(normalize=True).to_dict())
@@ -31,14 +33,36 @@ class NB_Classifier:
 	def gaussian_liklihood(self, x, mean, std):
 		return math.e**(-(x-mean)**2./(2.*std**2.))/(std*math.sqrt(2.*math.pi))
 	
-	def predict(self, abul=['f','f','f','f','f','f','f','f','t','f','f','f','l','f','n','f','f','t','f','f','f','t','f','t','f','t','f','f','f','f','f','f','f','t','t','n']):
+	def predict(self, data_tuple):
 		class_probabilities = dict()
 		for a_class in self.classes:
 			class_probabilities[a_class] = 1.
-			for i, value in enumerate(abul):
-				class_probabilities[a_class]*=self.summary[a_class][i][value]
-		print(class_probabilities)	
+			for i, value in enumerate(data_tuple):
+				if value in self.summary[a_class][i]:
+					class_probabilities[a_class]*=self.summary[a_class][i][value]
+				else:
+					class_probabilities[a_class]*=1e-8
+				
+		return max(class_probabilities, key=class_probabilities.get)	
 
+	def calc_accuracy(self):
+		self.predicted = []
+		for index, row in self.test.iloc[:,:-1].iterrows():
+			self.predicted.append(self.predict(row))
+		self.comparison = pd.DataFrame({'predicted' : self.predicted, 'actual' : self.test.iloc[:,-1]})
+		
+		self.comparison['res'] = self.comparison.apply(lambda x: 1 if x['predicted']==x['actual'] else 0, axis=1)
+		self.comparison['res'].value_counts(normalize=True)[1]
+
+	def calc_precision_recall(self):
+		self.cm = confusion_matrix(self.test.iloc[:,-1], self.predicted, self.test.iloc[:,-1].unique())
+		self.recall_classwise = np.diag(self.cm) / (np.sum(self.cm, axis = 1) + 1.e-8)  
+		self.precision_classwise = np.diag(self.cm) / (np.sum(self.cm, axis = 0) + 1.e-8)
+		self.recall = np.mean(self.recall_classwise)
+		self.precision = np.mean(self.precision_classwise)
+		self.f1 = 2.*self.precision*self.recall/(self.precision+self.recall)
+		pprint(self.cm)
+		print(self.precision, self.recall, self.f1)
 
 nb = NB_Classifier()
 
@@ -47,4 +71,5 @@ nb.read_dataset()
 nb.summerize_by_class()
 
 pprint(nb.summary)
-print(nb.predict())
+nb.calc_accuracy()
+nb.calc_precision_recall()
